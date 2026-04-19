aee7eec7120562d0b526df2a9b968c19102708eb
Sanchit Gupta <sanchitgupta2512@gmail.com>
2026-04-19 18:43:07 +0530

    correcting sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,15 +3,15 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://calculator-hub-brown.vercel.app',
+      url: 'https://calculator-hub-brown.vercel.app/',
       lastModified: new Date(),
     },
     {
-      url: 'https://calculator-hub-brown.vercel.app/calculators/battery-backup',
+      url: 'https://calculator-hub-brown.vercel.app/calculators/battery-backup/',
       lastModified: new Date(),
     },
     {
-      url: 'https://calculator-hub-brown.vercel.app/calculators/paint',
+      url: 'https://calculator-hub-brown.vercel.app/calculators/paint/',
       lastModified: new Date(),
     },
   ]
